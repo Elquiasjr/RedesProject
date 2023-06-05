30adac786db99ebf6e26b10cf513ce21bc161055
Elquiasjr <elquiasj@gmail.com>
2023-06-04 23:16:32 -0300

    feat: now covers more numbers of host, til 4 networks of /10

--- classes/IpDinamicDigits.ts
+++ classes/IpDinamicDigits.ts
@@ -1,10 +1,11 @@
 export class IpDinamicDigits{
-    public network: number; 
+    public network1: number;
+    public network2: number; 
     public host: number;
 
-    constructor(network: number, host: number){
-        this.network = network;
+    constructor(network1: number, network2: number, host: number){
+        this.network1 = network1;
+        this.network2 = network2;
         this.host = host;
     }
-
 }
